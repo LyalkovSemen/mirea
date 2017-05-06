5fbb60677f737d8cc755d8759e90cf595c19cf24
LyalkovSemen <semen-rab@mail.ru>
2017-05-06 19:10:55 +0300

    first commit

---
+++ labs_9_sem/lab_3_RegExp/RegExp/MainForm.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace RegExp
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.SplitContainer = new System.Windows.Forms.SplitContainer();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbRegExp = new System.Windows.Forms.TextBox();
+            this.tbInput = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbOutput = new System.Windows.Forms.TextBox();
+            this.comboBox = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.SplitContainer)).BeginInit();
+            this.SplitContainer.Panel1.SuspendLayout();
+            this.SplitContainer.Panel2.SuspendLayout();
+            this.SplitContainer.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // SplitContainer
+            // 
+            this.SplitContainer.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.SplitContainer.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.SplitContainer.Location = new System.Drawing.Point(0, 0);
+            this.SplitContainer.Name = "SplitContainer";
+            this.SplitContainer.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // SplitContainer.Panel1
+            // 
+            this.SplitContainer.Panel1.BackColor = System.Drawing.Color.LightGray;
+            this.SplitContainer.Panel1.Controls.Add(this.comboBox);
+            this.SplitContainer.Panel1.Controls.Add(this.label3);
+            this.SplitContainer.Panel1.Controls.Add(this.label1);
+            this.SplitContainer.Panel1.Controls.Add(this.tbRegExp);
+            this.SplitContainer.Panel1.Controls.Add(this.tbInput);
+            this.SplitContainer.Panel1MinSize = 180;
+            // 
+            // SplitContainer.Panel2
+            // 
+            this.SplitContainer.Panel2.BackColor = System.Drawing.Color.LightGray;
+            this.SplitContainer.Panel2.Controls.Add(this.label2);
+            this.SplitContainer.Panel2.Controls.Add(this.tbOutput);
+            this.SplitContainer.Panel2MinSize = 80;
+            this.SplitContainer.Size = new System.Drawing.Size(584, 462);
+            this.SplitContainer.SplitterDistance = 300;
+            this.SplitContainer.SplitterWidth = 3;
+            this.SplitContainer.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(9, 183);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(189, 21);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Регулярное выражение:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(8, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(124, 21);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Входная строка";
+            // 
+            // tbRegExp
+            // 
+            this.tbRegExp.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbRegExp.BackColor = System.Drawing.Color.Bisque;
+            this.tbRegExp.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tbRegExp.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tbRegExp.Location = new System.Drawing.Point(11, 248);
+            this.tbRegExp.Multiline = true;
+            this.tbRegExp.Name = "tbRegExp";
+            this.tbRegExp.Size = new System.Drawing.Size(561, 43);
+            this.tbRegExp.TabIndex = 2;
+            // 
+            // tbInput
+            // 
+            this.tbInput.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbInput.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tbInput.Font = new System.Drawing.Font("Calibri", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tbInput.Location = new System.Drawing.Point(11, 25);
+            this.tbInput.Multiline = true;
+            this.tbInput.Name = "tbInput";
+            this.tbInput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbInput.Size = new System.Drawing.Size(562, 147);
+            this.tbInput.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(8, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 21);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Результат";
+            // 
+            // tbOutput
+            // 
+            this.tbOutput.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbOutput.BackColor = System.Drawing.SystemColors.Window;
+            this.tbOutput.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tbOutput.Font = new System.Drawing.Font("Calibri", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tbOutput.Location = new System.Drawing.Point(11, 37);
+            this.tbOutput.Multiline = true;
+            this.tbOutput.Name = "tbOutput";
+            this.tbOutput.ReadOnly = true;
+            this.tbOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbOutput.Size = new System.Drawing.Size(562, 110);
+            this.tbOutput.TabIndex = 1;
+            // 
+            // comboBox
+            // 
+            this.comboBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.comboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox.Font = new System.Drawing.Font("Calibri", 12.75F);
+            this.comboBox.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.comboBox.FormattingEnabled = true;
+            this.comboBox.Location = new System.Drawing.Point(11, 207);
+            this.comboBox.Name = "comboBox";
+            this.comboBox.Size = new System.Drawing.Size(562, 34);
+            this.comboBox.TabIndex = 5;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(584, 462);
+            this.Controls.Add(this.SplitContainer);
+            this.DoubleBuffered = true;
+            this.ForeColor = System.Drawing.SystemColors.Control;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(600, 300);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "RegExp";
+            this.SplitContainer.Panel1.ResumeLayout(false);
+            this.SplitContainer.Panel1.PerformLayout();
+            this.SplitContainer.Panel2.ResumeLayout(false);
+            this.SplitContainer.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.SplitContainer)).EndInit();
+            this.SplitContainer.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer SplitContainer;
+        private System.Windows.Forms.TextBox tbInput;
+        private System.Windows.Forms.TextBox tbOutput;
+        private System.Windows.Forms.TextBox tbRegExp;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox;
+    }
+}
+
---
+++ labs_9_sem/lab_3_RegExp/RegExp/MainForm.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace RegExp
+{
+    public partial class MainForm : Form
+    {
+        public List<string> RegsList = new List<string>();
+        public List<string> TestList = new List<string>();
+        public MainForm()
+        {
+            InitializeComponent();
+            
+            //понеслась душа в рай
+            RegsList = LoadFromFile("Regs.txt");
+            TestList = LoadFromFile("Test.txt");
+            comboBox.DataSource = LoadFromFile("RegsNames.txt");
+            
+
+
+            if (comboBox.DataSource != null && RegsList != null)
+                comboBox.SelectedIndex = 0;
+            else
+                ErrorMessage();
+        }
+
+        //построчное чтение файла
+        public List<string> LoadFromFile(string FileName)
+        {
+            List<string> StringList = new List<string>();
+            string line;
+            try
+            {
+                var file = new System.IO.StreamReader(FileName);
+                while ((line = file.ReadLine()) != null)
+                {
+                    StringList.Add(line);
+                }
+                file.Close();
+                return StringList;
+            }
+            catch (Exception)
+            {return null;}
+            
+        }
+
+        //изменение комбобокса
+        private void cb_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox.DataSource != null && RegsList != null)
+                tbRegExp.Text = RegsList[comboBox.SelectedIndex];
+            if (TestList != null)
+                tbInput.Text = TestList[comboBox.SelectedIndex];
+        }
+        
+        //вывод ошибок
+        public void ErrorMessage()
+        {
+            MessageBox.Show("Что-то пошло не так...", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        
+    }//
+}//
