8b86ae8a3fbd484d6141178028d43fcfa0329326
LyalkovSemen <semen-rab@mail.ru>
2017-05-07 15:28:07 +0300

    lab1 start

---
+++ labs_9_sem/lab_1_Massive/Massives/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Massives
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
